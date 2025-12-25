f1166ee0688eea4cfb49567afafa772887c7cbcd
bryankiriama <bryankiriama@gmail.com>
2025-12-25 22:58:16 +0300

    debugged main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,22 @@ import Projects from './components/Projects'
 import Contact from './components/Contact'
 import Footer from './components/Footer'
 
+// function App() {
+//   return (
+//     <div className="bg-black text-white min-h-screen">
+//       <Hero />
+//       <About />
+//       <Skills />
+//       <Projects />
+//       <Contact />
+//       <Footer />
+//     </div>
+//   )
+// }
+
+// export default App
 function App() {
-  return (
-    <div className="bg-black text-white min-h-screen">
-      <Hero />
-      <About />
-      <Skills />
-      <Projects />
-      <Contact />
-      <Footer />
-    </div>
-  )
+  return <h1>Hello World</h1>
 }
 
 export default App
